1eed3565388f576f3ce38a2950474ab53b1dc54c
VenGenetic <fernando18avila.es@gmail.com>
2026-01-03 16:58:55 -0500

    mejora codigo 1.10

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Importar
+// CAMBIO AQUÍ: Importa HashRouter en lugar de BrowserRouter
+import { HashRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.tsx'
 import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* Envolver todo */}
+    {/* CAMBIO AQUÍ: Usa HashRouter */}
+    <HashRouter> 
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
