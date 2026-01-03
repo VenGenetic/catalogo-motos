547b7c11790693424100e02bd877e5f6db998234
VenGenetic <fernando18avila.es@gmail.com>
2026-01-03 17:08:09 -0500

    mejora codigo 1.11

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// CAMBIO AQUÍ: Importa HashRouter en lugar de BrowserRouter
+// CAMBIO IMPORTANTE: Usamos HashRouter en lugar de BrowserRouter
 import { HashRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.tsx'
@@ -8,7 +8,7 @@ import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* CAMBIO AQUÍ: Usa HashRouter */}
+    {/* CAMBIO: HashRouter es obligatorio para que funcione en Vercel sin configuraciones extra */}
     <HashRouter> 
       <CartProvider>
         <App />
