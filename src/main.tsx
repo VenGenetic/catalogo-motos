2d9da338a20ed25dd67a83d33aff1a3f5074fd01
VenGenetic <fernando18avila.es@gmail.com>
2026-01-04 01:57:43 -0500

    mejora codigo 1.46

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'; // <--- Importante
+import { BrowserRouter } from 'react-router-dom' // <--- ESTO FALTABA
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Envolvemos la App para que funcione el SEO */}
     <HelmetProvider>
-      <App />
+      <BrowserRouter> {/* <--- Importante: El Router debe envolver a la App */}
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
 )
