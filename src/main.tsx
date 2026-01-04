2780a672516fd4651c2dc20f961ad7fbcdace1ca
VenGenetic <fernando18avila.es@gmail.com>
2026-01-04 06:10:30 -0500

    mejora codigo 1.56

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CartProvider } from './context/CartContext' // <--- Importamos el carrito
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Envolvemos la App con el CartProvider para que funcione */}
+    <CartProvider>
+      <App />
+    </CartProvider>
   </StrictMode>,
 )
